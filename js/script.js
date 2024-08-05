066e4ce978e4720fbbd052486ae9d309ee20f9e0
Emmanueloye <esunday963@gmail.com>
2024-08-05 22:02:59 +0100

    update

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@ const box = document.querySelector('#color-box');
 const btn = document.querySelector('#change-color-btn');
 const heading = document.querySelector('h1');
 
+// Generates random number inclusive of the max number specified.
 const randomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
+// Generate random color based on the random number
 const randomColor = function () {
   return `rgb(${randomNumber(0, 255)}, ${randomNumber(0, 255)}, ${randomNumber(
     0,
@@ -13,6 +15,7 @@ const randomColor = function () {
   )})`;
 };
 
+// Handle the click event to change color on button click.
 document.addEventListener('DOMContentLoaded', () => {
   btn.addEventListener('click', () => {
     const color = randomColor();
